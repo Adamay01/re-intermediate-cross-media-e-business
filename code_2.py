daf1c8da6d9b27607f8239673e94ef71f08c9b45
Ada <139880994+Adamay01@users.noreply.github.com>
2023-10-27 01:08:54 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,50 @@
+if __name__ == "__main__":
+def main():
+
+def generate_random_data():
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    for item in data:
+
+        print(f"Random Number: {item}")
+    main()
+
+    return data        for (int item : data) {
+}
+        int[] data = new int[10];
+public class RandomDataGenerator {
+    }
+            System.out.println("Random Number: " + item);
+        }
+
+
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;}
+    }
+        int[] data = new int[10];
+        for (int item : data) {
+
+        }
+public class RandomDataGenerator {
+        }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+
+        for (int i = 0; i < 10; i++) {        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+}
+        }
+        }
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+    }
+
+public class RandomDataGenerator {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
